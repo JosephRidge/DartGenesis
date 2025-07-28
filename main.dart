b989e0ed57e42b80349fdb37b0655f19e1a151c7
JosephRidge <josephridge18@gmail.com>
2025-07-28 11:25:26 +0300

    The Genesis

---
+++ main.dart
@@ -0,0 +1,56 @@
+/*
+ * starting point of your dart program
+ * */
+void main() {  
+  
+ /*
+  * Dart is an OOP language 
+  * Data Type: defines where the data belongs to
+  * - Numbers (int, double)
+  * - Strings ('' or "")
+  * - Booleans (true or false)
+  * - Collections*( list, set, map)
+  * */
+  
+  // integers 0 to infinity and 0 to -ve infinity
+ 
+//   num y = 120; // infers to the actual type 
+//  double v = 120.65; // infers to the actual type 
+ int age = 140;
+ 
+//   Strings
+String animalName ="Lion";
+String animalChildName ='Cub';
+bool isNameEmpty =true; 
+  
+print("=====================================");
+print(animalName);
+print(animalChildName);
+//    string concatenation
+print("I am a "+animalName +" and my child is called junior "+ animalChildName);
+  
+print("I am a \$${animalName.toUpperCase()} and my child is called junior  ${animalChildName}. FYI, I am $age years old");
+  
+print("===================================");
+   
+}
+ 
+
+
+
+
+/*
+ * BUDGET program based on fundamentals learnt
+ * (variables, data types & arithmetic) 
+ * Expenses
+ * Income
+ * estimateDryDate
+ * planForFuture( ) // save based on income
+ 
+ * */
+
+
+
+
+
+ 
